82bd8b68e01e4be602a3a3a197ca3cc74dceb782
Rafael Gustavo Simon Werner <rafaelgustavowerner@gmail.com>
2020-10-24 22:55:54 -0300

    Adiciona categorias

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
-    has_many :albums
+  has_many :albums
 
-    validates :name, presence: true
+  validates :name, presence: true
 
-    has_one_attached :image
+  has_one_attached :image
 end
