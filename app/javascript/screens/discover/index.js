8309f4c8aee46c1df9369f0200a569331e1bbe9c
Rafael Gustavo Simon Werner <rafaelgustavowerner@gmail.com>
2020-11-08 17:52:18 -0300

    Adicionado react

---
+++ app/javascript/screens/discover/index.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const DiscoverScreen = () => {
+  return(
+    <Fragment>
+      <div>Discover Screen</div>
+    </Fragment>
+  )
+}
+
+export default DiscoverScreen;
