c3a8a26965942c4c99c740d9c5edcfb051cac809
Rafael Gustavo Simon Werner <rafaelgustavowerner@gmail.com>
2020-11-08 23:58:17 -0300

    Adiciona Navbar

--- app/javascript/screens/discover/index.js
+++ app/javascript/screens/discover/index.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from 'react';
+import SectionWrapper from '../../components/common/section_wrapper';
+import Disccovery from '../../components/discovery';
+import NavbarFooter from '../../components/common/navbar_footer';
 
 const DiscoverScreen = () => {
   return(
     <Fragment>
-      <div>Discover Screen</div>
+      <SectionWrapper>
+        <Disccovery />
+      </SectionWrapper>
+      <NavbarFooter />
     </Fragment>
-  )
+  );
 }
 
 export default DiscoverScreen;
